07095731a6c7b304a1fa0ba152ec988c45160025
Dev1-web <nervesparksdev@gmail.com>
2025-06-15 10:56:32 +0530

    fix

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 # Step 1: Install Python packages
 pip install -r requirements.txt
 
-# Step 2: Install only Chromium browser
+# Step 2: Install Playwright and browsers with system dependencies
+python -m playwright install-deps
 python -m playwright install chromium
