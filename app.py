7279c88f09547402a02525451cf170d7ffeb4e57
Varun Sharma <nervesparksdev03@gmail.com>
2025-06-19 16:36:41 +0000

    final

--- app.py
+++ app.py
@@ -25,8 +25,7 @@ st.set_page_config(
 )
 
 # Backend API URL
-API_BASE_URL = "http://35.209.111.61/api1/"
-"soup"
+API_BASE_URL = "http://35.206.118.178/api1/"
 
 # Custom CSS for better styling
 st.markdown("""
